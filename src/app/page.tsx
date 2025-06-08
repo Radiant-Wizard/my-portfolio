9a2a842964f8b8872354c0d3cd28c25245d6347d
radiant_wizard <hei.taratra@gmail.com>
2025-06-08 21:04:46 +0300

    feat: added the tech stack component to the main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Stack from "@/components/UI/Stack";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <div className="">
         <Hero/>
         <About/>
+        <Stack/>
       </div>
     </main >
   );
