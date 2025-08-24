4036480ed1c99842e738db650dd673a2c2070539
Radiant-Wizard <hei.taratra@gmail.com>
2025-08-24 12:16:35 +0300

    chore(page.tsx): refactored the main page to follow a cleaner structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <Hero />
-    </>
+    <div className="min-h-screen">
+      <Header/>
+      <main>
+        <Hero/>
+      </main>
+
+      
+    </div>
+
   );
 }
