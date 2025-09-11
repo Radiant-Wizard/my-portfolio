a883f5e61f65b665cf605b1f7a698286d63aa928
Radiant-Wizard <hei.taratra@gmail.com>
2025-09-11 15:36:25 +0300

    chore(page): added Contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,19 @@ import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Projects } from "@/components/Projects";
 import { Skills } from "@/components/Skills";
+import { Contact } from "@/components/Contact";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Header/>
+      <Header />
       <main>
-        <Hero/>
-        <About/>
-        <Skills/>
-        <Projects/>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
       </main>
-
-      
     </div>
-
   );
 }
